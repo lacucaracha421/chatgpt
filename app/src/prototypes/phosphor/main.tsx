49625efbf10bf6d0d27ee8dd6c755313585bde6c
lacucaracha421 <269853277+lacucaracha421@users.noreply.github.com>
2026-09-06 13:55:17 +0900

    chore: remove retired phosphor prototype

--- app/src/prototypes/phosphor/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { Phosphor } from './Phosphor';
-
-// Later: <Phosphor selectedAssetUrls={selectedAssets.map(asset => asset.url)} onExit={closeGame} />
-createRoot(document.getElementById('root')!).render(<Phosphor />);
